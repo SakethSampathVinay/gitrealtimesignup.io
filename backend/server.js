3e28df90c4fa83cbc37435e06db4c2d91828abff
Saketh Sampath Vinay <sakethsampath2006@gmail.com>
2024-08-12 23:25:27 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = 'mongodb+srv://SakethSampathVinay:YvWh3*YYb$!u88q@cluster0.mnx6q.mongodb.net/RealTimeLoginSignUp';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.post('/api/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).send('User created');
+  } catch (error) {
+    res.status(500).send('Signup failed');
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
